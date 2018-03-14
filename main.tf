0213d0b616fce84cbf3e18376aa6604ee0071361
daniel <daniel.hermann@fastleansmart.com>
2018-03-14 19:54:55 +0100

    change trigger logic

--- main.tf
+++ main.tf
@@ -75,7 +75,6 @@ data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = "${data.null_data_source.check_script_change_trigger.outputs["source_dir"]}"
   output_path = "${path.module}\\lambda\\${var.stack_prefix}-${var.unique_name}.zip"
-  depends_on  = ["local_file.buildlambdazip"]
 }
 
 # Create lambda function
